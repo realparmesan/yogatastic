cc0ba7e64b6ed5c91d7a6b436dd921094c5536d8
Chris Chester <chrisjchester@gmail.com>
2020-02-01 08:58:08 +1300

    Set-up basic Hugo + Webpack + TS

---
+++ webpack.common.js
@@ -0,0 +1,60 @@
+const webpack = require("webpack");
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const AssetsPlugin = require("assets-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    main: path.join(__dirname, "src", "index.ts"),
+    // cms: path.join(__dirname, "src", "js", "cms.js"),
+  },
+  devtool: 'inline-source-map',
+  output: {
+    path: path.join(__dirname, "dist")
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.((png)|(eot)|(woff)|(woff2)|(ttf)|(svg)|(gif))(\?v=\d+\.\d+\.\d+)?$/,
+        use: ["file-loader?name=/[hash].[ext]"]
+      },
+      {
+        test: /\.tsx?$/,
+        use: [{
+            loader: "ts-loader",
+        }],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        exclude: /node_modules/,
+        use: ["style-loader", MiniCssExtractPlugin.loader, "css-loader", "postcss-loader", "sass-loader"]
+      }
+    ]
+  },
+
+  plugins: [
+    new AssetsPlugin({
+      filename: "webpack.json",
+      path: path.join(process.cwd(), "site/data"),
+      prettyPrint: true
+    }),
+    new CopyWebpackPlugin([
+      {
+        from: "./src/fonts/",
+        to: "fonts/",
+        flatten: true
+      }
+    ]),
+    // new HtmlWebpackPlugin({
+    //   filename: 'admin/index.html',
+    //   template: 'src/cms.html',
+    //   inject: false,
+    // }),
+  ]
+};
