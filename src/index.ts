c33b73c8b1bc2c32bcd1da71714f6f1f809ea949
Chris Chester <chrisjchester@gmail.com>
2020-02-02 08:10:32 +1300

    Ts actually working

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,2 @@
 import * as _ from 'lodash';
-function component() {
-    const element = document.createElement('div');
-
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-    return element;
-}
-
-document.body.appendChild(component());
+console.log("test 2?");
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,8 @@
 const merge = require("webpack-merge");
 const path = require("path");
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const WriteFilePlugin = require("write-file-webpack-plugin");
 
 const common = require("./webpack.common");
 
@@ -10,7 +11,8 @@ module.exports = merge(common, {
 
   output: {
     filename: "[name].js",
-    chunkFilename: "[id].css"
+    chunkFilename: "[id].css",
+    publicPath: "./dist"
   },
 
   devServer: {
@@ -29,12 +31,13 @@ module.exports = merge(common, {
       cleanOnceBeforeBuildPatterns: [
         "dist/**/*.js",
         "dist/**/*.css",
-        "site/data/webpack.json"
+        "data/webpack.json"
       ]}),
 
     new MiniCssExtractPlugin({
       filename: "[name].css",
       chunkFilename: "[id].css"
-    })
+    }),
+    new WriteFilePlugin()
   ]
 });
